4fab10fba7dd7934ba7dca6cf77debaeb6b2cd4b
Gulchachak <1gulchachak05@mail.ru>
2023-09-25 23:50:22 +0300

    add array

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+string[] input = {"Иж", "Ока", "Волга", "Казанка", "Дон"}; 
+string[] output = {}; 
+ 
+for (int i = 0; i < input.Length; i++) { 
+    string s = input[i]; 
+    if (s.Length <= 3) { 
+        output = output.Append(s).ToArray(); 
+    } 
+} 
+ 
+Console.WriteLine("[{0}]", string.Join(", ", output));
